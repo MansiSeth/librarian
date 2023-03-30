0e9c386fcf5d6530db38b76a24897b7158902efa
mansi <81612581+MansiSeth@users.noreply.github.com>
2023-03-30 15:52:07 +0530

    router fix after removing about and home vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 
 import books from '@/views/Books.vue'
@@ -13,14 +12,6 @@ const routes = [
     path: '/',
     name: 'login',
     component: () =>import("../views/Login.vue")
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   },
   {
     path: '/books',
